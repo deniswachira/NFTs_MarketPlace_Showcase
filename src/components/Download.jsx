29a5fe6dc4d9a98ad9baee103264d7a76a4b7d13
deniswachira <deniswachira77@gmail.com>
2022-04-07 21:52:14 +0300

    attached links

--- src/components/Download.jsx
+++ src/components/Download.jsx
@@ -7,10 +7,11 @@ const Download = () => {
     <div className={`${styles.section} ${styles.bgWhite}`}>
       <div className={`${styles.subSection} flex-col text-center`}>
             <div>
-              <h1 className={`${styles.h1Text} ${styles.blackText}`}>Download the Source Code</h1>
+              <h1 className={`${styles.h1Text} ${styles.blackText}`}>Download the APK file</h1>
+              <button className={styles.btnPrimary}>  <a href="https://expo.dev/artifacts/267aa632-6394-42fd-8cb7-82106d329cc4">Download APK</a></button>
               <h1 className={`${styles.pText} ${styles.blackText}`}>Get the full source code on GitHub</h1>
             </div>
-            <button className={styles.btnPrimary}>Source Code</button>
+            <button className={styles.btnPrimary}><a href="https://github.com/deniswachira/NFT_Marketplace_MobileApp">Source Code</a></button>
             <div className={styles.flexCenter}>
               <img  src={assets.scene} alt="download_png" className={styles.fullImg}/>
             </div>
